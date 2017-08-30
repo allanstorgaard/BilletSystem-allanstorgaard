5c48bec99f5656622696c97c8826ec7de9af0bc9
Allan Storgaard <allanstorgaard@outlook.dk>
2017-08-30 12:00:43 +0200

    Oprettet DLL

---
+++ BilletLibrary/Bill.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLibrary
+{
+    public class Bill
+    {
+        //Properties
+        public string Nummerplade { get; set; }
+        public DateTime Dato { get; set; }
+
+        //Methods
+        public int Pris(int pris)
+        {
+            return pris;
+        }
+
+        public string Køretøj()
+        {
+            return "Bil";
+        }
+
+    }
+}
---
+++ BilletLibraryTests/BillTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLibrary.Tests
+{
+    [TestClass()]
+    public class BillTests
+    {
+        [TestMethod()]
+        public void PrisTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void KøretøjTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
